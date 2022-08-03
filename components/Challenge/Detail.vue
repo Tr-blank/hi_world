c9bbb6ae95f16189231954d5281e933919267da0
Tr-blank <blank0129@gmail.com>
2022-08-03 22:24:12 +0800

    feat: HW-94 adjust challange detail form

--- components/Challenge/Detail.vue
+++ components/Challenge/Detail.vue
@@ -2,6 +2,28 @@
   <div class="">
     <form>
       <UiText v-model:value="formData.title" label="title" title="標題" />
+      <UiText v-model:value="formData.title" label="title" title="說明" />
+      <div>
+        <span class="block">是否公開</span>
+        <label><input type="radio" value="1" name="isPublic" />是</label>
+        <label><input type="radio" value="0" name="isPublic" />否</label>
+      </div>
+      <div>
+        <span class="block">標籤</span>
+        <select>
+          <option>標籤1</option>
+          <option>標籤2</option>
+        </select>
+      </div>
+      <div>
+        <span class="block">模式</span>
+        <select>
+          <option v-for="model in challengeModelList" :value="model.key">
+            {{ model.name }}
+          </option>
+        </select>
+      </div>
+      <UiText v-model:value="formData.title" label="title" title="備註" />
       <div class="pt-4">
         <button type="button" class="border px-4 py-1 rounded mr-4" :disabled="isOriginalFormData" @click="saveFormData">儲存</button>
         <button type="button" class="border px-4 py-1 rounded" :disabled="isCreateForm" @click="deleteChallenge">刪除</button>
@@ -10,8 +32,8 @@
   </div>
 </template>
 <script setup> 
-  import { ref, computed, watch } from 'vue' 
   import UiText from '@/components/Ui/Text.vue'
+
   const props = defineProps({
     isCreateForm: Boolean,
     form: Object
@@ -19,6 +41,8 @@
   const emit = defineEmits(['save-challenge', 'delete-challenge'])
   const { form } = toRefs(props)
   let formData = ref({})
+  const { fetchChallengeModelList, challengeModelList } = useChallengeModel()
+  fetchChallengeModelList()
   const isOriginalFormData = computed(() => JSON.stringify(formData.value) === JSON.stringify(form.value))
   const saveFormData = () => {
     emit('save-challenge', formData.value)
