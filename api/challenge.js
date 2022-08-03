ab951724aff3c0e236435a513c1f216cc2c8492d
Tr-blank <blank0129@gmail.com>
2022-08-03 22:23:24 +0800

    feat: HW-99 adjust api name

--- api/challenge.js
+++ api/challenge.js
@@ -1,77 +1,77 @@
 export const getChallengeList = () => {
-  return $fetch('http://localhost:3000/api/challenge/list', {
+  return $fetch('http://localhost:3000/api/challenge/challenge-list', {
     method: 'get'
   })
 }
 
 export const addChallenge = (body) => {
-  return $fetch('http://localhost:3000/api/challenge/add', {
+  return $fetch('http://localhost:3000/api/challenge/add-challenge', {
     method: 'post',
     body
   })
 }
 
 export const updateChallenge = (id, body) => {
-  return $fetch(`http://localhost:3000/api/challenge/update?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/update-challenge?id=${id}`, {
     method: 'post',
     body
   })
 }
 
 export const delChallenge = (id) => {
-  return $fetch(`http://localhost:3000/api/challenge/delete?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/delete-challenge?id=${id}`, {
     method: 'get'
   })
 }
 
 export const getChallengeModelList = () => {
-  return $fetch('http://localhost:3000/api/challenge-model/list', {
+  return $fetch('http://localhost:3000/api/challenge/challenge-model-list', {
     method: 'get'
   })
 }
 
 export const addChallengeModel = (body) => {
-  return $fetch('http://localhost:3000/api/challenge-model/add', {
+  return $fetch('http://localhost:3000/api/challenge/add-challenge-model', {
     method: 'post',
     body
   })
 }
 
 export const updateChallengeModel = (id, body) => {
-  return $fetch(`http://localhost:3000/api/challenge-model/update?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/update-challenge-model?id=${id}`, {
     method: 'post',
     body
   })
 }
 
 export const delChallengeModel = (id) => {
-  return $fetch(`http://localhost:3000/api/challenge-model/delete?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/delete-challenge-model?id=${id}`, {
     method: 'get'
   })
 }
 
 export const getChallengeTagList = () => {
-  return $fetch('http://localhost:3000/api/challenge-tag/list', {
+  return $fetch('http://localhost:3000/api/challenge/challenge-tag-list', {
     method: 'get'
   })
 }
 
 export const addChallengeTag = (body) => {
-  return $fetch('http://localhost:3000/api/challenge-tag/add', {
+  return $fetch('http://localhost:3000/api/challenge/add-challenge-tag', {
     method: 'post',
     body
   })
 }
 
 export const updateChallengeTag = (id, body) => {
-  return $fetch(`http://localhost:3000/api/challenge-tag/update?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/update-challenge-tag?id=${id}`, {
     method: 'post',
     body
   })
 }
 
 export const delChallengeTag = (id) => {
-  return $fetch(`http://localhost:3000/api/challenge-tag/delete?id=${id}`, {
+  return $fetch(`http://localhost:3000/api/challenge/delete-challenge-tag?id=${id}`, {
     method: 'get'
   })
 }
