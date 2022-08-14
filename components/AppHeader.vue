1c0ffe88c81b891b7e29aa518cdbcaa38bb00473
Tr-blank <blank0129@gmail.com>
2022-08-15 00:19:52 +0800

    feat: HW-112 add user property info at user center page

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,15 @@
 <template>
   <header class="p-4 bg-gray-100">
-    <div class="max-w-7xl w-full mx-auto flex">
-      <h1><nuxt-link to="/">logo</nuxt-link></h1>
-      <nav class="flex px-4">
-        <nuxt-link to="/challenge">挑戰</nuxt-link>
-      </nav>
+    <div class="max-w-7xl w-full mx-auto flex justify-between">
+      <div class="flex">
+        <h1><nuxt-link to="/">logo</nuxt-link></h1>
+        <nav class="flex px-4">
+          <nuxt-link to="/challenge">挑戰</nuxt-link>
+        </nav>
+      </div>
+      <div>
+        <nuxt-link to="/user">登入</nuxt-link>
+      </div>
     </div>
   </header>
 </template>
--- layouts/user-center.vue
+++ layouts/user-center.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="layout flex">
     <aside class="h-screen w-30 bg-blue-200 text-center flex flex-col justify-between">
-      <header class="p-4">logo</header>
+      <header class="p-4"><nuxt-link to="/">logo</nuxt-link></header>
       <nav class="py-10">
         <a href="" class="block px-4 py-1">挑戰</a>
         <a href="" class="block px-4 py-1">系統</a>
@@ -14,14 +14,35 @@
       </header>
       <slot />
     </div>
-    <aside class="w-60 border-l">
+    <aside class="w-60 border-l flex flex-col justify-between">
       <header class="p-4">
-        <div class="text-right">會員</div>
+        <div class="text-right">
+          <div>{{ userProperty.name }}</div>
+          <div class="text-sm">@{{ userProperty.account }}</div>
+        </div>
       </header>
+      <footer class="p-4">
+        <div>遊戲幣: {{ userProperty.inGameMoney }}</div>
+        <div>代幣: {{ userProperty.tokenMoney }}</div>
+      </footer>
     </aside>
   </div>
 </template>
 
 <script setup>
-// your setup script
+  import { getUserPropertyByAccount } from '@/api/user'
+  const localTestAccount = 'tiya12345'
+  const userProperty = ref({})
+  const fetchUserProperty = async () => {
+    try {
+      const { data } = await getUserPropertyByAccount(localTestAccount)
+      userProperty.value = data[0]
+    } catch (error) {
+      console.debug(error)
+    }
+  }
+  onMounted(async () => {
+    await fetchUserProperty()
+  })
+
 </script>
