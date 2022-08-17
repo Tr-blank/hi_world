4992d5ee0b3addebd3dd4560b7d2f78a5f733e23
Tr-blank <blank0129@gmail.com>
2022-08-17 21:31:22 +0800

    feat: HW-114 add shop tag api

---
+++ api/shop.js
@@ -0,0 +1,51 @@
+export const getShopList = () => {
+  return $fetch('http://localhost:3000/api/shop/shop-list', {
+    method: 'get'
+  })
+}
+
+export const addShop = (body) => {
+  return $fetch('http://localhost:3000/api/shop/add-shop', {
+    method: 'post',
+    body
+  })
+}
+
+export const updateShop = (id, body) => {
+  return $fetch(`http://localhost:3000/api/shop/update-shop?id=${id}`, {
+    method: 'post',
+    body
+  })
+}
+
+export const delShop = (id) => {
+  return $fetch(`http://localhost:3000/api/shop/delete-shop?id=${id}`, {
+    method: 'get'
+  })
+}
+
+export const getShopTagList = () => {
+  return $fetch('http://localhost:3000/api/shop/shop-tag-list', {
+    method: 'get'
+  })
+}
+
+export const addShopTag = (body) => {
+  return $fetch('http://localhost:3000/api/shop/add-shop-tag', {
+    method: 'post',
+    body
+  })
+}
+
+export const updateShopTag = (id, body) => {
+  return $fetch(`http://localhost:3000/api/shop/update-shop-tag?id=${id}`, {
+    method: 'post',
+    body
+  })
+}
+
+export const delShopTag = (id) => {
+  return $fetch(`http://localhost:3000/api/shop/delete-shop-tag?id=${id}`, {
+    method: 'get'
+  })
+}
---
+++ server/api/shop/shop-tag-list.ts
@@ -0,0 +1,12 @@
+import { queryByCollection } from "../../lib/firestore"
+import { handleSort } from "../../lib/useData"
+
+export default defineEventHandler (async (event) => {
+  try {
+    const docs = await queryByCollection('shopTag')
+    const data = handleSort(docs, 'desc', 'updateDate')
+    return { status: 200, data}
+  } catch (error) {
+    return { status: 400, message: error.message, error }
+  }
+})
